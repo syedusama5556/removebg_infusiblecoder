0043562b77acd79f254cbd3496ecbbf9fc8cd519
syedusama5556 <syedusama12345@gmail.com>
2024-03-11 13:00:20 +0500

    fixed version

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,6 +1,8 @@
 @echo off
 set /p id=Enter Commit Message: 
+set /p version=Enter Version Number: 
 git add .
 git commit -m "%id%"
-git push origin main
+git tag -a v%version% -m "Version %version% release - %id%"
+git push origin main --tags
 timeout 2
