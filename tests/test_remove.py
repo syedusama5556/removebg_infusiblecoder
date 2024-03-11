9f82f9414b9e2e40074f98a8b0632f46ddd1dc4b
syedusama5556 <syedusama12345@gmail.com>
2024-03-11 12:46:05 +0500

    fixed tests

--- tests/test_remove.py
+++ tests/test_remove.py
@@ -1,20 +1,65 @@
 from io import BytesIO
 from pathlib import Path
 
-from imagehash import average_hash
+from imagehash import phash as hash_img
 from PIL import Image
 
-from removebg_infusiblecoder import remove
+from removebg_infusiblecoder import new_session, remove
 
 here = Path(__file__).parent.resolve()
 
-
 def test_remove():
-    image = Path(here / ".." / "examples" / "animal-1.jpg").read_bytes()
-    expected = Path(here / ".." / "examples" / "animal-1.out.png").read_bytes()
-    actual = remove(image)
+    kwargs = {
+        "sam": {
+            "anime-girl-1" : {
+                "sam_prompt" :[{"type": "point", "data": [400, 165], "label": 1}],
+            },
 
-    actual_hash = average_hash(Image.open(BytesIO(actual)))
-    expected_hash = average_hash(Image.open(BytesIO(expected)))
+            "car-1" : {
+                "sam_prompt" :[{"type": "point", "data": [250, 200], "label": 1}],
+            },
 
-    assert actual_hash == expected_hash
+            "cloth-1" : {
+                "sam_prompt" :[{"type": "point", "data": [370, 495], "label": 1}],
+            },
+
+            "plants-1" : {
+                "sam_prompt" :[{"type": "point", "data": [724, 740], "label": 1}],
+            },
+        }
+    }
+
+    for model in [
+        "u2net",
+        "u2netp",
+        "u2net_human_seg",
+        "u2net_cloth_seg",
+        "silueta",
+        "isnet-general-use",
+        "isnet-anime",
+        "sam"
+    ]:
+        for picture in ["anime-girl-1", "car-1", "cloth-1", "plants-1"]:
+            image_path = Path(here / "fixtures" / f"{picture}.jpg")
+            image = image_path.read_bytes()
+
+            actual = remove(image, session=new_session(model), **kwargs.get(model, {}).get(picture, {}))
+            actual_hash = hash_img(Image.open(BytesIO(actual)))
+
+            expected_path = Path(here / "results" / f"{picture}.{model}.png")
+            # Uncomment to update the expected results
+            # f = open(expected_path, "wb")
+            # f.write(actual)
+            # f.close()
+
+            expected = expected_path.read_bytes()
+            expected_hash = hash_img(Image.open(BytesIO(expected)))
+
+            print(f"image_path: {image_path}")
+            print(f"expected_path: {expected_path}")
+            print(f"actual_hash: {actual_hash}")
+            print(f"expected_hash: {expected_hash}")
+            print(f"actual_hash == expected_hash: {actual_hash == expected_hash}")
+            print("---\n")
+
+            assert actual_hash == expected_hash
