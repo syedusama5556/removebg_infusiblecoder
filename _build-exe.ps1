87bc6917aed6a3ce1acc58a8afa4a0841e1b0baf
syedusama5556 <syedusama12345@gmail.com>
2024-03-12 07:24:38 +0500

    fixed

--- _build-exe.ps1
+++ _build-exe.ps1
@@ -1,8 +1,8 @@
 # Install required packages
 pip install pyinstaller
 pip install -e ".[cli]"
-pip uninstall typing
-pip uninstall pathlib
+pip uninstall typing -y
+pip uninstall pathlib -y
 # Create PyInstaller spec file with specified data collections
 # pyi-makespec --collect-data=gradio_client --collect-data=gradio removebg_infusiblecoder.py
 
